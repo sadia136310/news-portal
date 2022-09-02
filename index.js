8d1c7ac32be95e56bbf17a3daa14023657760b7b
sadia136310 <sadia1363@gmail.com>
2022-09-03 00:51:41 +0600

    dynamic data updated

--- index.js
+++ index.js
@@ -10,15 +10,84 @@ const loadDataDisplay = (datas) => {
 
     const newsContainer = document.getElementById('news-container');
     datas.forEach(data => {
-        console.log(data);
+        // console.log(data);
 
         const { category_name } = data;
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
-             <h5>${category_name}</h5>
+             <h5 onclick="loadDataDetails()">${category_name}</h5>
+             
         `;
         newsContainer.appendChild(newsDiv)
 
     });
 }
+const loadDataDetails = () => {
+    url = 'https://openapi.programming-hero.com/api/news/category/01'
+    fetch(url)
+        .then(response => response.json())
+        .then(data => loadDataDetailsDisplay(data.data))
+}
+
+const loadDataDetailsDisplay = (datas) => {
+    // console.log(datas);
+    const cardContainer = document.getElementById('card-container');
+    datas.forEach(data => {
+        console.log(data);
+
+        const { thumbnail_url, title, details, image_url, author, total_view } = data
+        cardDiv = document.createElement('div');
+        cardDiv.innerHTML = `
+         <div class="card mb-5">
+                <div class="row g-0 shadow-lg">
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="${thumbnail_url}" class="img-fluid rounded-start p-4 shadow-lg" alt="...">
+                    </div>
+                    <div class="col-lg-9 col-sm-6">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">${details}</p>
+                             
+
+                           <div class="d-flex g-5">
+                             <div>
+                             <img src="${author.img}" class="img-fluid rounded-circle" style="width: 6rem;"alt="...">
+                             </div>
+
+                            <div class="p-3">
+                             <p class="card-text fw-bold"> ${author.name}</p>
+                             <p class="card-text text-muted"> ${author.published_date}</p>
+                            </div>
+
+                         <div  class="p-3">
+                          <p class="card-text fw-bold">view : ${total_view}</p>
+                         </div>
+                          <div  class="p-3">
+                         <i class="fa-solid fa-star text-warning"></i>
+                         <i class="fa-regular fa-star text-warning"></i>
+                         <i class="fa-regular fa-star text-warning"></i>
+                         <i class="fa-regular fa-star text-warning"></i>
+                         <i class="fa-regular fa-star text-warning"></i>
+                         </div>
+
+                          <div  class="p-3">
+                           
+                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"    data-bs-target="#exampleModal">
+                       See Details
+                       </button>
+                          </div>  
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(cardDiv);
+    })
+}
+const modalDetails = () => {
+
+}
+
+loadDataDetails();
 dataLoad();
