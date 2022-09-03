cf9f70be76e2244874cbdaa1754a4f07e4f4abb5
sadia136310 <sadia1363@gmail.com>
2022-09-03 18:56:47 +0600

    bonus

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dataLoad = () => {
     fetch(url)
         .then(response => response.json())
         .then(data => loadDataDisplay(data.data.news_category))
+        .catch(error => console.log(error))
 }
 const loadDataDisplay = (datas) => {
 
@@ -15,12 +16,13 @@ const loadDataDisplay = (datas) => {
         const { category_name } = data;
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
-             <h5 onclick="loadDataDetails('${data.category_id}')">${category_name}</h5>
+             <h5 onclick="loadDataDetails('${data.category_id}')"> ${category_name} </h5>
         `;
         newsContainer.appendChild(newsDiv);
 
     });
 }
+
 
 const loadDataDetails = (id) => {
     // console.log(id);
@@ -28,24 +30,46 @@ const loadDataDetails = (id) => {
     fetch(url)
         .then(response => response.json())
         .then(data => loadDataDetailsDisplay(data.data))
+        .catch(error => console.log(error))
 }
+
 
 const loadDataDetailsDisplay = (datas) => {
     console.log(datas);
+
+    datas.sort((a, b) => {
+        return b.rating.number - a.rating.number;
+    });
+
+
+    const messageFound = document.getElementById('message-found');
+    messageFound.innerText = ''
+    const messageDiv = document.createElement('div')
+    messageDiv.innerHTML = `
+    <h3> ${datas.length === 0 ? "no" : datas.length} items are found </h3>
+    `;
+    messageFound.appendChild(messageDiv);
+
+
+
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
+
+
     datas.forEach(data => {
         // console.log(data);
-
-        const { thumbnail_url, title, details, image_url, author, total_view } = data
+        const { thumbnail_url, title, details, image_url, author, rating } = data
         cardDiv = document.createElement('div');
+
+
+
         cardDiv.innerHTML = `
          <div class="card mb-5">
                 <div class="row g-0 shadow-lg">
-                    <div class="col-lg-3 col-sm-6">
+                    <div class="col-lg-4 col-sm-6">
                         <img src="${thumbnail_url}" class="img-fluid rounded-start p-4 shadow-lg" alt="...">
                     </div>
-                    <div class="col-lg-9 col-sm-6">
+                    <div class="col-lg-8 col-sm-6">
                         <div class="card-body">
                             <h5 class="card-title">${title}</h5>
                             <p class="card-text">${details.length > 400 ? details.slice(0, 400) + '...' : details}</p>
@@ -59,10 +83,10 @@ const loadDataDetailsDisplay = (datas) => {
                              <p class="card-text text-muted"> ${author.published_date}</p>
                             </div>
                          <div  class="p-3">
-                          <p class="card-text fw-bold">view : ${total_view}</p>
+                          <p class="card-text fw-bold"><i class="fa-solid fa-eye"></i> ${rating.number}</p>
                          </div>
                           <div  class="p-3">
-                         <i class="fa-solid fa-star text-warning"></i>
+                         <i class="fa-solid fa-star text-warning "></i>
                          <i class="fa-regular fa-star text-warning"></i>
                          <i class="fa-regular fa-star text-warning"></i>
                          <i class="fa-regular fa-star text-warning"></i>
@@ -84,6 +108,7 @@ const loadDataDetailsDisplay = (datas) => {
     })
 }
 
+
 const modalDetails = (item) => {
 
     url = `https://openapi.programming-hero.com/api/news/${item}`
@@ -91,6 +116,7 @@ const modalDetails = (item) => {
     fetch(url)
         .then(response => response.json())
         .then(data => modalDetailsDisplay(data.data))
+        .catch(error => console.log(error))
 }
 
 const modalDetailsDisplay = (datas) => {
@@ -99,10 +125,12 @@ const modalDetailsDisplay = (datas) => {
     modalSection.innerHTML = '';
 
     datas.forEach(data => {
-        const { thumbnail_url, author, total_view } = data
+        const { thumbnail_url, author, total_view, title } = data
 
         const modalDiv = document.createElement('div');
         modalDiv.innerHTML = `
+
+        
       <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
          <p class="card-text fw-bold">Author name: ${author.name === null ? 'name not found ' : author.name}</p>
          <p class="card-text fw-bold">view: ${total_view === 0 ? 'not found' : total_view}</p>
